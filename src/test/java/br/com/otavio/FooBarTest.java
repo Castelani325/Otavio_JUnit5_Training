d69d79261a092c18b51b136453b8b5ab2c5b6188
Otavio Vidal <otavio.castelani@gmail.com>
2025-12-28 11:56:42 -0400

    first commit

---
+++ src/test/java/br/com/otavio/FooBarTest.java
@@ -0,0 +1,10 @@
+package br.com.otavio;
+
+import org.junit.jupiter.api.Test;
+
+public class teste01 {
+
+    @Test
+    void name() {
+    }
+}
---
+++ src/test/java/br/com/otavio/SimpleMathTest.java
@@ -0,0 +1,4 @@
+package br.com.otavio;
+
+public class SimpleMathTest {
+}
