8e1b0517fb1857c53c90a86b57f78016b00edf22
Otavio Vidal <otavio.castelani@gmail.com>
2025-12-30 23:32:02 -0400

    Criado file para exemplificar um test com a anotação @MethodeOrder para  a sequencia de tests do file ser aleatórias

---
+++ src/test/java/br/com/otavio/MethodOrderedRandonlyTest.java
@@ -0,0 +1,52 @@
+package br.com.otavio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandonlyTest {
+
+    @Test
+    //@DisplayName("Test description")
+    void testA() {
+        System.out.println("Testando Test A");
+        //Given()
+        //When()
+        //Then()
+
+    }
+
+    @Test
+    //@DisplayName("Test description")
+    void testB() {
+        System.out.println("Testando Test B");
+        //Given()
+        //When()
+        //Then()
+
+    }
+
+    @Test
+    //@DisplayName("Test description")
+    void testC() {
+        System.out.println("Testando Test C");
+        //Given()
+        //When()
+        //Then()
+
+    }
+
+    @Test
+    //@DisplayName("Test description")
+    void testD() {
+        System.out.println("Testando Test D");
+        //Given()
+        //When()
+        //Then()
+
+            }
+
+}
